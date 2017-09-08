e2acc613453ec11da9b6d12a637238a95f58881e
Ekaaaaaaaaaaaaaaaa <bmc.ginn@aol.com>
2017-09-09 03:44:31 +0800

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -5,7 +5,7 @@ google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><sc
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,220);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,440);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,1);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 80
+google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 100
 killall chrome
 killall chrome
 killall chrome

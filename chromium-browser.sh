d07e9835fe3caf99fa92e80e012f6d9b48b3d303
Ekaaaaaaaaaaaaaaaa <bmc.ginn@aol.com>
2017-09-11 05:11:00 +0800

    Create chromium-browser.sh

---
+++ chromium-browser.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+while :
+do
+chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
+chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,220);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
+chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,440);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
+chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 120
+killall chromium-browser
+killall chromium-browser
+killall chromium-browser
+apt-get clean
+sleep 20
+done

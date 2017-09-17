f817e8c976bda5d3f49186977d0cc60208e261d5
Ekaaaaaaaaaaaaaaaa <bmc.ginn@aol.com>
2017-09-18 02:25:34 +0800

    Update chromium-browser.sh

--- chromium-browser.sh
+++ chromium-browser.sh
@@ -1,18 +1,19 @@
 !/bin/bash
 while :
 do
-chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
-chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,220);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
+chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
+chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,220);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
 chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,440);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
-chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,1);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
-chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(620,1);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
+chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,1);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
+chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(620,1);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
 chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(930,1);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
-chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(930,220);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
-chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(620,220);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
-chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 100
+chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(930,220);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
+chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(620,220);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
+chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
+chromium-browser --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(930,440);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 100
 killall chromium-browser
 killall chromium-browser
 killall chromium-browser
 apt-get clean
-sleep 10
+sleep 15
 done

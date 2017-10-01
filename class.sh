22e5861df9e38458174635943bf66342a73ed509
Ekaaaaaaaaaaaaaaaa <bmc.ginn@aol.com>
2017-10-02 00:36:17 +0800

    Update class.sh

--- class.sh
+++ class.sh
@@ -1,20 +1,2 @@
-!/bin/bash
-while :
-do
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,200);window.location='https://t.co/YK1ycmelXQ';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,220);window.resizeTo(300,200);window.location='https://t.co/YK1ycmelXQ';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 1
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,440);window.resizeTo(300,200);window.location='https://t.co/YK1ycmelXQ';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,1);window.resizeTo(300,200);window.location='https://t.co/YK1ycmelXQ';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(620,1);window.resizeTo(300,200);window.location='https://t.co/YK1ycmelXQ';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 1
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(930,1);window.resizeTo(300,200);window.location='https://t.co/YK1ycmelXQ';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(930,220);window.resizeTo(300,200);window.location='https://t.co/YK1ycmelXQ';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(620,220);window.resizeTo(300,200);window.location='https://t.co/YK1ycmelXQ';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,220);window.resizeTo(300,200);window.location='https://t.co/YK1ycmelXQ';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(930,440);window.resizeTo(300,200);window.location='https://t.co/YK1ycmelXQ';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='https://t.co/YK1ycmelXQ';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 100
-killall chrome
-killall chrome
-killall chrome
-apt-get clean
-sleep 9
-done
+104.193.108.142 webdesign-inspiration.com
+104.193.108.142 www.webdesign-inspiration.com

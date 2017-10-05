ea9847e2b0a8b48057fbf5c5c601e3b79715dc2f
Ekaaaaaaaaaaaaaaaa <bmc.ginn@aol.com>
2017-10-06 05:11:48 +0800

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,10 +1,10 @@
 !/bin/bash
 while :
 do
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,220);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,440);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='http://blogger.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 120
+google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,200);window.location='http://thormotorcoach.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
+google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,220);window.resizeTo(300,200);window.location='http://thormotorcoach.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
+google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,440);window.resizeTo(300,200);window.location='http://thormotorcoach.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
+google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='http://thormotorcoach.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 100
 killall chrome
 killall chrome
 killall chrome
